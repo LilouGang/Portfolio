6bae470616e838e38dc4ac66a4f8b509944bece0
Killian Lacaque <killian@MacBook-Air-de-Killian.local>
2025-11-24 11:32:52 +0100

    Ajout de la balise meta

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="Y65NE1Berxi8y4Gcny87MqE1PKChyKA9Se47RSHBp7Y" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link 
